96dfde7181cf7347ca8f1c6a63e361625b143033
Patrick Roberts <patrob22@gmail.com>
2023-03-17 20:09:14 -0400

    changed answer func to display text on webpage

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ const submit = document.getElementById('button');
 const questionBox = document.getElementById('textbox');
 
 const answer = () => {
+    let text;
     if (questionBox.value.length > 0) {
         let num = Math.floor(Math.random() * 8);
         if (num === 0) {
-            alert('It is all but certain!');
+            text = 'It is all but certain!';
         } else if (num === 1) {
-            alert('It is most likely to pass!');
+            text = 'It is most likely to pass!';
         } else if (num === 2) {
-            alert('It is possible! Be patient.');
+            text = 'It is possible! Be patient.';
         } else if (num === 3) {
-            alert('It is hard to tell for certain, but the signs are in your favor.')
+            text = 'It is hard to tell for certain, but the signs are in your favor.'
         } else if (num === 4) {
-            alert('The signs are unclear around your question.  Try asking again later.');
+            text = 'The signs are unclear around your question.  Try asking again later.';
         } else if (num === 5) {
-            alert('It does not look promising my friend.')
+            text = 'It does not look promising my friend.'
         } else if (num === 6) {
-            alert('I would not hold out too much hope.')
+            text = 'I would not hold out too much hope.'
         } else if (num === 7) {
-            alert('I am sorry to say that it is very unlikely.')
+            text = 'I am sorry to say that it is very unlikely.'
         };
     } else {
-        alert('Please enter a question.');
+        text = 'Please enter a question.';
     };
+    document.getElementById('reveal').textContent = text;
 };
 
 submit.addEventListener('click', answer);
